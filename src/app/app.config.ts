130ffc772d748778f905af0ad4425f9521a48294
Ayaan <ayaanakbar08@gmail.com>
2024-04-28 20:31:15 -0700

    April 28

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([authInterceptor]))
+
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HelloComponent } from './hello/hello.component';
 import { ParentCompaniesComponent } from './parent-companies/parent-companies.component';
 import { ParentCompaniesSubsidiariesComponent } from './parent-companies/parent-companies-subsidiaries.component';
+import { LoginComponent } from './auth/login.component';
 
 export const routes: Routes = [
     {path:'', component:HelloComponent, pathMatch: 'full'},
     {path:'ParentCompanies', component:ParentCompaniesComponent},
-    {path: 'parentCompaniesSubsidiaries/:id', component:ParentCompaniesSubsidiariesComponent}
+    {path: 'parentCompaniesSubsidiaries/:id', component:ParentCompaniesSubsidiariesComponent},
+    {path: 'login', component:LoginComponent}
 ];
